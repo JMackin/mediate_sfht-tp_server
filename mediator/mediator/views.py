065af758faa4e57a17b4b3d816d6607251573a72
John Mackin <johnluthermackin@jlmw.xyz>
2023-06-01 19:54:00 -0500

    Fixed views and urls

--- mediator/mediator/views.py
+++ mediator/mediator/views.py
@@ -2,9 +2,9 @@ import os
 
 from django.shortcuts import render
 from sftp_machine import connection_agent as sftpDoer
-
-
-# Most (maybe all) of the views will involve interaction with SFTP_machine, which instantiates an
+# from mediator import objects as objs
+import mediator as md
+# Most (maybe all) of the views wil involve interaction with SFTP_machine, which instantiates an
 # object to make connections and send SFTP commands. As of now every request will create and destroy
 # a new instance of SFTP_Doer. This is horribly inefficient so...
 # TODO: Implement caching or sessions to persist SFTP_Doer instances
@@ -12,8 +12,8 @@ from sftp_machine import connection_agent as sftpDoer
 
 def index(request):
 
-    return render(request, os.fspath("objects/templates/index.html"))
+    return render(request, os.fspath("index.html"), content_type="text/html")
 
 
 def books_indx(request):
-    return render(request, os.fspath("objects/books.html"))
+    return render(request, os.fspath("books.html"), content_type="text/html")
